c657005fb98be0ca5cbf11e7ecb871c8144c0fcd
Horus Lugo <horusgoul@gmail.com>
2020-10-10 17:56:54 +0200

    fix(create-nginx-conf.sh): fix AND operator in if statements

--- create-nginx-conf.sh
+++ create-nginx-conf.sh
@@ -8,7 +8,7 @@ fi
 
 PERISCOPE_CONFIG=""
 
-if [ "$RTMP_PERISCOPE_ENABLED" == "true" && "$RTMP_PERISCOPE_KEY" != "" ]; then
+if [ "$RTMP_PERISCOPE_ENABLED" == "true" ] && [ "$RTMP_PERISCOPE_KEY" != "" ]; then
   PERISCOPE_CONFIG="
     # Periscope transcoding
     exec ffmpeg -i rtmp://127.0.0.1/live/\$name -crf 30 -preset ultrafast -acodec aac -strict experimental -ar 44100 -ac 2 -b:a 64k -vcodec libx264 -x264-params keyint=60:no-scenecut=1 -r 30 -b:v 500k -s 1280x720 -f flv $RTMP_PERISCOPE_SERVER$RTMP_PERISCOPE_KEY;
@@ -17,7 +17,7 @@ fi
 
 TWITCH_CONFIG=""
 
-if [ "$RTMP_TWITCH_ENABLED" == "true" && "$RTMP_TWITCH_KEY" != "" ]; then
+if [ "$RTMP_TWITCH_ENABLED" == "true" ] && [ "$RTMP_TWITCH_KEY" != "" ]; then
   TWITCH_CONFIG="
     # Twitch
     push $RTMP_TWITCH_SERVER$RTMP_TWITCH_KEY;
@@ -26,7 +26,7 @@ fi
 
 YOUTUBE_CONFIG=""
 
-if [ "$RTMP_YOUTUBE_ENABLED" == "true" && "$RTMP_YOUTUBE_KEY" != "" ]; then
+if [ "$RTMP_YOUTUBE_ENABLED" == "true" ] && [ "$RTMP_YOUTUBE_KEY" != "" ]; then
   YOUTUBE_CONFIG="
     # YouTube
     push $RTMP_YOUTUBE_SERVER$RTMP_YOUTUBE_KEY;
